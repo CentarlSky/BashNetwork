f64327de702694daca09f92660fc1c053088fa00
CentarlSky <133049031+CentarlSky@users.noreply.github.com>
2023-08-27 01:33:26 +0900

    MainBash.sh

--- MainBash.sh
+++ MainBash.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Remote server to ping (replace with an appropriate server)
+
 remote_server="google.com"
 
-# Perform a ping test
+
 ping_result=$(ping -c 4 "$remote_server")
 
-# Check if the ping was successful
+
 if [ $? -eq 0 ]; then
     echo "Wi-Fi connection is working. Ping results for $remote_server:"
     echo "$ping_result"
